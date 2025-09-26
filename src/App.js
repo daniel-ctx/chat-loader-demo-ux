adc593e9db34feb48f657b625c05ff1b3d8db02d
daniel-design-ctx <daniel.carvalho@cortex-intelligence.com>
2025-09-26 13:37:27 -0300

    build

--- src/App.js
+++ src/App.js
@@ -9,9 +9,12 @@ function App() {
   return <div className='main'>
     <div className='text-description'>
       <div>
-        <h1>Chat response</h1>
+        <h1>Chat loader </h1>
         <h2>Feature para melhoria de percepção do usuário sobre tempo e racional do AI</h2>
-        <div className='point'><IconArrowRight /><p>Tempo de resposta:{processingSeconds ? ` • ~${processingSeconds}s` : ''}</p></div>
+        <div className='point'><IconArrowRight /><p>Envie uma mensagem no chat para ver o comportamento de resposta do AI</p></div>
+        <div className='point'><IconArrowRight /><p>Para respostas curtas o AI vai exibir uma frase temporária</p></div>
+        <div className='point'><IconArrowRight /><p>Para respostas longas, o AI vai exibir o raciocínio em passos e subpassos</p></div>
+        <div className='point'><IconArrowRight /><p>Tempo de resposta da última mensagem para referencia:{processingSeconds ? ` • ~${processingSeconds}s` : ''}</p></div>
       </div>
     </div>
     <div className='content'><Chat onProcessingStart={() => {}} onProcessingEnd={(secs) => setProcessingSeconds(secs)} /></div>
